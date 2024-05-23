77a74121186e69420b22c3bb702f83385bf55f36
Yuliia Pavliuk <olga96855@gmail.com>
2024-05-23 09:31:58 +0300

    'added content on Info page'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Flex, Text, IconButton } from '@chakra-ui/react'
 import { FiMenu } from 'react-icons/fi'
+import { InfoPage } from '../components/InfoPage'
 
 export default function Home() {
-	return <>Index Page</>;
-  }
+	return <InfoPage />
+}
